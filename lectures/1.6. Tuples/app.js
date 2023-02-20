13bd70c840cdec5a9f4b934a929ebb1092319d32
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-20 18:38:18 +0500

    Tuples

---
+++ lectures/1.6. Tuples/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const skill = [1, 'Dev'];
+// const id = skill[0];
+// const skillName = skill[1];
+// skill.push('sdsdds');
+// const sdf = skill[2];
+// skill.pop();
+const [id, skillName] = skill;
+const arr = [1, 'sdf', true, true, false];
---
+++ lectures/1.6. Tuples/app.ts
@@ -0,0 +1,11 @@
+const skill: [number, string] = [1, 'Dev'];
+// const id = skill[0];
+// const skillName = skill[1];
+
+// skill.push('sdsdds');
+// const sdf = skill[2];
+// skill.pop();
+
+const [id, skillName] = skill;
+
+const arr: [number, string, ...boolean[]] = [1, 'sdf', true, true, false];
