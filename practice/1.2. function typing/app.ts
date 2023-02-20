c32b931ab1790b79360629331b9921c560edc00c
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-20 19:42:32 +0500

    Function typing

--- practice/1.2. function typing/app.ts
+++ practice/1.2. function typing/app.ts
@@ -1,21 +1,23 @@
-let info: {
-	officeId: number,
-	isOpened: boolean,
-	contacts: {
-		phone: string,
-		email: string,
-		address: {
-			city: string
-		}
-	} 
-} = {
-	"officeId": 45,
-	"isOpened": false,
-	"contacts": {
-		"phone": "+79100000000",
-		"email": "my@email.ru",
-		"address": {
-			"city": "Москва"
-		}
-	}
+enum QuestionStatus {
+	Published = 'published',
+	Draft = 'draft',
+	Deleted = 'deleted',
+}
+
+async function getFaqs(req: {
+	topicId: number;
+	status: QuestionStatus
+}): Promise<{
+	question: string;
+	answer: string;
+	tags: string[];
+	likes: number;
+	status: QuestionStatus;
+}[]> {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
 }
