2c4a1e55656b086ebfd90992d5db71ab316176c9
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-14 19:10:47 +0500

    Union

---
+++ lectures/2.1 Union/app.js
@@ -0,0 +1,20 @@
+"use strict";
+function logId(id) {
+    if (typeof id === 'string') {
+        console.log(id);
+    }
+    else if (typeof id === 'number') {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
+}
+function logError(err) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    }
+    else {
+        console.log(err);
+    }
+}
