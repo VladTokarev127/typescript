93cee26b52b76ad0ceecb06d1cadad452c7d8395
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-14 19:39:11 +0500

    Types or Interfaces

---
+++ lectures/2.5 Types or Interfaces/app.ts
@@ -0,0 +1,18 @@
+interface User {
+	name: string
+}
+
+interface User {
+	age: number
+}
+
+const user: User = {
+	name: 'as',
+	age: 33
+}
+
+type ID = string | number;
+
+interface IDI {
+	ID: string | number
+}
