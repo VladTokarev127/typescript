d3c2c74e34a58f4be86c6e3760ae2c906b6c1e86
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-14 19:33:59 +0500

    Interfaces

---
+++ lectures/2.4 Interfaces/app.ts
@@ -0,0 +1,46 @@
+interface User {
+	name: string,
+	age: number,
+	skills: string[]
+
+	log: (id: number) => string;
+}
+
+interface Role {
+	roleId: number;
+}
+
+interface UserWithRole extends User, Role {
+	createdAt: Date;
+}
+
+type User2 = {
+	name: string,
+	age: number,
+	skills: string[]
+
+	log: (id: number) => string;
+}
+
+
+let user: UserWithRole = {
+	name: 'asd',
+	age: 33,
+	skills: ['1', '2'],
+	roleId: 1,
+	createdAt: new Date(),
+
+	log(id) {
+		return '';
+	}
+}
+
+interface UserDic {
+	[index: number]: User,
+}
+
+type UserDic2 = {
+	[index: number]: User,
+}
+
+type ud = Record<number, User>;
