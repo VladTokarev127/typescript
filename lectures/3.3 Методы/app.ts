ec35cacc730d03f00c617a072eb30fe251ddc2d0
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-19 13:11:34 +0500

    Методы

---
+++ lectures/3.3 Методы/app.ts
@@ -0,0 +1,36 @@
+enum PaymentStatus {
+	Holded,
+	Processed,
+	Reversed
+}
+
+class Payment {
+	id: number;
+	status: PaymentStatus = PaymentStatus.Holded;
+	createdAt: Date = new Date();
+	updatedAt: Date;
+
+	constructor(id: number) {
+		this.id = id;
+		// this.createdAt = new Date();
+		// this.status = PaymentStatus.Holded;
+	}
+
+	getPaymentsLifeTime(): number {
+		return new Date().getTime() - this.createdAt.getTime();
+	}
+
+	unholdPayment(): void {
+		if (this.status === PaymentStatus.Processed) {
+			throw new Error('Платёж не может быть возвращен!');
+		}
+		this.status = PaymentStatus.Reversed;
+		this.updatedAt = new Date();
+	}
+}
+
+const payment = new Payment(1);
+payment.unholdPayment();
+console.log(payment);
+const time = payment.getPaymentsLifeTime();
+console.log(time);
