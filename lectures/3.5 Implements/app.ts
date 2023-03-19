d754513a4cce7e210a124d7504c025f34f3cbb26
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-19 14:02:24 +0500

    Implements

---
+++ lectures/3.5 Implements/app.ts
@@ -0,0 +1,33 @@
+interface ILogger {
+	log(...args: any[]): void;
+	error(...args: any[]): void;
+}
+
+class Logger implements ILogger {
+	log(...args: any[]): void {
+		console.log(...args);
+	}
+	async error(...args: any[]): Promise<void> {
+		// Кинуть во внешнюю систему
+		console.log(...args);
+	}
+}
+
+interface IPayable {
+	pay(paymentId: number): void;
+	price?: number;
+}
+
+interface IDeletable {
+	delete(): void;
+}
+
+class User implements IPayable, IDeletable {
+	delete(): void {
+		// throw new Error("Method not implemented.");
+	}
+	pay(paymentId: number | string): void {
+		// throw new Error("Method not implemented.");
+	}
+	price?: number | undefined;
+}
