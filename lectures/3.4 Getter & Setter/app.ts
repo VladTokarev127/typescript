8574c2492a91019042aa4a4972aa2bd081d65281
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-19 13:30:48 +0500

    Getter & Setter

---
+++ lectures/3.4 Getter & Setter/app.ts
@@ -0,0 +1,27 @@
+class User {
+	_login: string;
+	password: string;
+	createdAt: Date;
+
+	set login(l: string | number) {
+		this._login = 'user-' + l;
+		this.createdAt = new Date();
+	}
+
+	get login() {
+		return this._login;
+	}
+
+	async setPassword(p: string) {
+
+	}
+
+	// set password(p: string) {
+	// 	// sync
+	// }
+}
+
+const user = new User();
+user.login = 'myLogin';
+console.log(user);
+console.log(user.login);
