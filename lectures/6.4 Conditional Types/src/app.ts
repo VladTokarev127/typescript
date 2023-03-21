5bacb8884af1afdcd3b44e33cfba589f3516d19f
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-21 22:07:28 +0500

    Conditional Types

---
+++ lectures/6.4 Conditional Types/src/app.ts
@@ -0,0 +1,48 @@
+const a1: number = Math.random() > 0.5 ? 1 : 0;
+
+interface HTTPResponse<T extends 'success' | 'failed'> {
+	code: number;
+	data: T extends 'success' ? string : Error;
+}
+
+const suc: HTTPResponse<'success'> = {
+	code: 200,
+	data: 'done'
+};
+
+const err: HTTPResponse<'failed'> = {
+	code: 200,
+	data: new Error()
+}
+
+class User {
+	id: number;
+	name: string;
+}
+
+class UserPersistend extends User {
+	dbId: string;
+}
+
+function getUser(id: number): User;
+function getUser(dbId: string): UserPersistend;
+function getUser(dbIdOrId: string | number): User | UserPersistend {
+	if(typeof dbIdOrId === 'number') {
+		return new User();
+	} else {
+		return new UserPersistend();
+	}
+}
+
+type UserOrUserPersistend<T extends string | number> = T extends number ? User : UserPersistend;
+
+function getUser2<T extends string | number>(id: T): UserOrUserPersistend<T> {
+	if(typeof id === 'number') {
+		return new User() as UserOrUserPersistend<T>;
+	} else {
+		return new UserPersistend() as UserOrUserPersistend<T>;
+	}
+}
+
+const res1 = getUser2(1);
+const res2 = getUser2('12321312');
