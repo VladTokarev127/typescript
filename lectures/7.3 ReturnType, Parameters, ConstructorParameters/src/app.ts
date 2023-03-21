634f6e0ea48dc21c4018cced9327334861eb7d8c
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-21 23:45:50 +0500

    ReturnType, Parameters, ConstructorParameters

---
+++ lectures/7.3 ReturnType, Parameters, ConstructorParameters/src/app.ts
@@ -0,0 +1,17 @@
+class User {
+	constructor(public id: number, public name: string) { }
+}
+
+function getData(id: number): User {
+	return new User(id, 'Вася');
+}
+
+type RT = ReturnType<typeof getData>;
+type RT2 = ReturnType<() => void>;
+type RT3 = ReturnType<<T>() => T>;
+type RT4 = ReturnType<<T extends string>() => T>;
+
+type PT = Parameters<typeof getData>;
+type first = PT[0];
+
+type CP = ConstructorParameters<typeof User>;
