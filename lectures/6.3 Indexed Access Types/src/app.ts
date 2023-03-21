a182784501fb1799cac9019a17a7583db3e2d7bd
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-21 21:51:13 +0500

    Indexed Access Types

---
+++ lectures/6.3 Indexed Access Types/src/app.ts
@@ -0,0 +1,33 @@
+interface Role {
+	name: string;
+}
+
+interface Permission {
+	endDate: Date;
+}
+
+interface User {
+	name: string;
+	roles: Role[];
+	permission: Permission;
+}
+
+const user: User = {
+	name: 'Вася',
+	roles: [],
+	permission: {
+		endDate: new Date()
+	}
+}
+
+const nameUser = user['name'];
+let roleNames: 'roles' = 'roles'; // == const roleNames = 'roles';
+
+type rolesType = User['roles'];
+type rolesType2 = User[typeof roleNames];
+
+type roleType = User['roles'][number];
+type dateType = User['permission']['endDate'];
+
+const roles = ['admin', 'user', 'super-user'] as const;
+type roleTypes = typeof roles[number];
